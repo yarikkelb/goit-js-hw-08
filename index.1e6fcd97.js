62721d66ef980ae2cd709b9b439238a612752560
yarikkelb <125504558+yarikkelb@users.noreply.github.com>
2023-06-30 20:29:14 +0000

    Deploying to gh-pages from @ yarikkelb/goit-js-hw-08@4077c2bd547f03bb49d8181991d4280f929fb4b2 🚀

--- index.1e6fcd97.js
+++
@@ -1,2 +0,0 @@
-console.log("hello world"),console.log("hello world");
-//# sourceMappingURL=index.1e6fcd97.js.map
--- index.a51ecad2.js
+++
@@ -1,2 +0,0 @@
-console.log("hello world"),console.log("hello world");
-//# sourceMappingURL=index.a51ecad2.js.map
