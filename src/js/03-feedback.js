83a148973ecfc49c9488f89bd1e7ba51074679af
yarikkelb <125504558+yarikkelb@users.noreply.github.com>
2023-07-02 23:16:23 +0300

    2316

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,8 @@
-//import throttle from 'lodash.throttle';
-
+import throttle from 'lodash.throttle';
 const LOCAL_KEY = 'feedback-form-state';
-
 form = document.querySelector('.feedback-form');
-
-form.addEventListener('input', throttle(onInputData, 500));
-form.addEventListener('submit', onFormSubmit);
-
+//form.addEventListener('input', throttle(onInputData, 500));
+//form.addEventListener('submit', onFormSubmit);
 let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
 const { email, message } = form.elements;
 reloadPage();
